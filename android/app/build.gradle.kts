240d2b51fc7bde7e567c8d98d42acb088a7d6824
Edgar Rafael Jr. <202210027@fit.edu.ph>
2025-04-30 16:37:56 +0800

    added a splash screen, fixed some dependencies for release mode (end-users)  of the app, temporary app icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,6 +35,12 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+
+            // Enable code shrinking (R8/ProGuard)
+            isMinifyEnabled = true
+
+            // Use default proguard rules and add your custom proguard-rules.pro
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             
         }
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '/pages/home.dart';
+import 'pages/splash_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage() // This sets the HomePage fuction from home.dart as the home page of the app.
+      home: AnimatedSplashScreenWidget(),  // This sets the HomePage fuction from home.dart as the home page of the app.
     );
   }
 }
